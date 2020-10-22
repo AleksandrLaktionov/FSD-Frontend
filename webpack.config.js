f419da1f38563a38894ce33824ac0d7753261cc0
AleksandrLaktionov <72191399+AleksandrLaktionov@users.noreply.github.com>
2020-10-22 22:16:23 +0300

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,43 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        main: path.resolve(__dirname, './src/index.js'),
+    },
     output: {
         filename: '[name].js',
-        path: path.resolve(__dirname,'./dist'),
-        publicPath: 'dist'
+        path: path.resolve(__dirname, './dist')
     },
     module: {
         rules: [{
             test: /\.js$/,
-            use: {
-              loader: 'babel-loader',
-              exclude: '/node_modules/',
-              options: {
-                presets: ['@babel/preset-env'],
-                plugins: ['@babel/plugin-proposal-object-rest-spread']
-              }
-            }
-          }]
-      },
-    devServer: {
-        overlay: true
-    }
+            exclude: /node_modules/,
+            use: ['babel-loader'],
+        },
+        {
+            test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+            type: 'asset/resource',
+        },
+    ]},
+    // mode: 'development',
+    // devServer: {
+    //     historyApiFallback: true,
+    //     contentBase: path.resolve(__dirname, './dist'),
+    //     open: true,
+    //     compress: true,
+    //     hot: true,
+    //     port: 8080,
+    // },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'webpack Boilerplate',
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: '[name].html',
+        }),
+        new CleanWebpackPlugin(),
+        // new webpack.HotModuleReplacement(),
+    ],
 }
