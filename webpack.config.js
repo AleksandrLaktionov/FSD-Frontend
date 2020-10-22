ef2fce0973cbf9501c2f1be123bb27877c38eba8
AleksandrLaktionov <72191399+AleksandrLaktionov@users.noreply.github.com>
2020-10-22 14:42:31 +0300

    webpack plagin

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,19 @@ module.exports = {
         path: path.resolve(__dirname,'./dist'),
         publicPath: 'dist'
     },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            use: {
+              loader: 'babel-loader',
+              exclude: '/node_modules/',
+              options: {
+                presets: ['@babel/preset-env'],
+                plugins: ['@babel/plugin-proposal-object-rest-spread']
+              }
+            }
+          }]
+      },
     devServer: {
         overlay: true
     }
