a60067e04e76adce5652d5bc8419fcf13036e95b
AleksandrLaktionov <72191399+AleksandrLaktionov@users.noreply.github.com>
2020-12-02 21:18:17 +0300

    Radio Buttons

--- src/js/post.js
+++ src/js/post.js
@@ -1,14 +1,26 @@
-// import './../pug/UIkit/Checkbox'
+import {
+    Checkbox_List, 
+    Form_checkbox1, 
+    Form_checkbox2, 
+    Form_checkbox3, 
+    Form_checkbox4, 
+    Form_checkbox5, 
+    Form_checkbox6
+} from './../pug/UIkit/Checkbox'
 
 const Add = (a,b) => a+b
 
 export default Add
 
-// $(document).ready(function(){
-//     $('.Form10').click(function(){
-//         $(this).html([Checkbox_List, Form_checkbox])
-//     });
-// })
-// function Checkbox (){
-//     
-// }
+$(document).ready(function(){
+    ($('.Form10').click(function(){
+        $(this).html([
+            Checkbox_List, 
+            Form_checkbox1, 
+            Form_checkbox2, 
+            Form_checkbox3, 
+            Form_checkbox4, 
+            Form_checkbox5, 
+            Form_checkbox6])
+    }));
+})
--- src/pug/UIkit/Checkbox.js
+++ src/pug/UIkit/Checkbox.js
@@ -2,31 +2,39 @@ export const Checkbox_List = $(`
     <div class='Form11'>
         <p>expandable checkbox list</p>
     </div>`);
-export const Form_checkbox = $(`
-    <div class='Form_checkbox'>
-        <form>
-            <input type='checkbox' name='Checkbox1'/>
-            <label for='Checkbox1' id='Checkbox1'>Завтрак<label/>
+export const Form_checkbox1 = $(`
+        <form class='Form_checkbox1'>
+            <input type='checkbox' name='Checkbox1' id='Checkbox1_input'/>
+            <label for='Checkbox1' id='Checkbox1_label'>Завтрак<label/>
         </form>
-        <form>
-            <input type='checkbox' name='Checkbox2'/>
-            <label for='Checkbox2' id='Checkbox2'>Письменный стол<label/>
+`)
+export const Form_checkbox2 = $(`
+        <form class='Form_checkbox2'>
+            <input type='checkbox' name='Checkbox2' id='Checkbox2_input'/>
+            <label for='Checkbox2' id='Checkbox2_label'>Письменный стол<label/>
         </form>
-        <form>
-            <input type='checkbox' name='Checkbox3'/>
-            <label for='Checkbox3' id='Checkbox3'>Стул для кормления<label/>
+`)
+export const Form_checkbox3 = $(`
+        <form class='Form_checkbox3'>
+            <input type='checkbox' name='Checkbox3' id='Checkbox3_input'/>
+            <label for='Checkbox3' id='Checkbox3_label'>Стул для кормления<label/>
         </form>
-        <form>
-            <input type='checkbox' name='Checkbox4'/>
-            <label for='Checkbox4' id='Checkbox4'>Кроватка<label/>
+`)
+export const Form_checkbox4 = $(`
+        <form class='Form_checkbox4'>
+            <input type='checkbox' name='Checkbox4' id='Checkbox4_input'/>
+            <label for='Checkbox4' id='Checkbox4_label'>Кроватка<label/>
         </form>
-        <form>
-            <input type='checkbox' name='Checkbox5'/>
-            <label for='Checkbox5' id='Checkbox5'>Телевизор<label/>
+`)
+export const Form_checkbox5 = $(`
+        <form class='Form_checkbox5'>
+            <input type='checkbox' name='Checkbox5' id='Checkbox5_input'/>
+            <label for='Checkbox5' id='Checkbox5_label'>Телевизор<label/>
         </form>
-        <form>
-            <input type='checkbox' name='Checkbox6'/>
-            <label for='Checkbox6' id='Checkbox6'>Шампунь<label/>
+`)
+export const Form_checkbox6 = $(`
+        <form class='Form_checkbox6'>
+            <input type='checkbox' name='Checkbox6' id='Checkbox6_input'/>
+            <label for='Checkbox6' id='Checkbox6_label'>Шампунь<label/>
         </form>
-    </div>
 `)
